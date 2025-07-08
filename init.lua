a9c33db9c09cd4534ca913625fab9d92e939f9c6
Erli Kuka <ekuka21@epoka.edu.al>
2025-07-08 21:38:07 +0200

    chore: updates

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 require("erli.core.options")
 require("erli.core.remaps")
+require("erli.core.space")
 require("erli.lazy")
+
 
---
+++ lua/erli/core/space.lua
@@ -0,0 +1,19 @@
+-- Function to set tab and indentation
+local set_indent = function(ft, tabstop, shiftwidth, expandtab)
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = ft,
+    callback = function()
+      vim.bo.tabstop = tabstop
+      vim.bo.shiftwidth = shiftwidth
+      vim.bo.expandtab = expandtab
+    end,
+  })
+end
+
+-- TypeScript & JavaScript: 2 spaces
+set_indent({ "typescript", "typescriptreact", "javascript", "javascriptreact" }, 2, 2, true)
+
+-- Python: 4 spaces
+set_indent("python", 4, 4, true)
+
+
